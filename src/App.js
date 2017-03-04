1e8f7ae93cb33aff943cebea52540a358bc7faa8
Tinus Wagner <tinus@3months.com>
2017-03-04 13:37:07 +1300

    boom, declare those widths outside of your component and simply call em

--- src/App.js
+++ src/App.js
@@ -23,6 +23,18 @@ const list = [
 const isSearched = (searchTerm) => (item) =>
   !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
+const largeColumn = {
+  width: '40%',
+};
+
+const midColumn = {
+  width: '30%',
+};
+
+const smallColumn = {
+  width: '10%',
+};
+
 class App extends Component {
 
   constructor(props) {
@@ -83,13 +95,13 @@ const Table = ({ list, pattern, onDismiss }) =>
   <div className="table">
     { list.filter(isSearched(pattern)).map(item =>
       <div key={item.objectID} className="table-row">
-        <span style={{ width: '40%' }}>
+        <span style={largeColumn}>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span style={{ width: '30%' }}>{item.author}</span>
-        <span style={{ width: '10%' }}>{item.num_comments}</span>
-        <span style={{ width: '10%' }}>{item.points}</span>
-        <span style={{ width: '10%' }}>
+        <span style={midColumn}>{item.author}</span>
+        <span style={smallColumn}>{item.num_comments}</span>
+        <span style={smallColumn}>{item.points}</span>
+        <span style={smallColumn}>
           <Button onClick={() => onDismiss(item.objectID)} className="button-inline"> 
             Dismiss
           </Button>
